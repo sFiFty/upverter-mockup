29004c5ffb9daade2e70baaff7ebbe0e91752327
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-06-27 18:48:27 +0300

    imported another images

--- src/components/SideContent/SideContent.js
+++ src/components/SideContent/SideContent.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import './styles.css';
+import browser from '../../images/browser.png';
 
 class SideContent extends Component {
   render() {
     return (
-      <div className="side-content">
+      <div className="side-content d-flex flex-column justify-content-center">
+        <div className="image-container">
+          <img src={browser} className="browser-img" alt="browser" />
+        </div>
       </div>
     );
   }
