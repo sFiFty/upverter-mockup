46f1b215a9ca1fc84371f72e278ada8d59ab434c
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-06-27 18:59:54 +0300

    added text

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -5,6 +5,12 @@ class MainContent extends Component {
   render() {
     return (
       <div className="main-content">
+        <div className="text-content">
+          <h1>Share, screenshot and print.</h1>
+          <div className="subhead">
+            Upverter Viewer makes it easy to share views of your design.
+          </div>
+        </div>
       </div>
     );
   }
