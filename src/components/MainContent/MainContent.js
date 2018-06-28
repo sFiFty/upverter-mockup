b54cbdff8d374747c49ede4c282e982290d5098e
sFiFty <alexandr.rudin.my@gmail.com>
2018-06-28 19:59:51 +0300

    finished text

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import './styles.css';
 import iconReplay from '../../images/icon-replay.svg';
+import iconPlus from '../../images/icon-plus.svg';
 
 class MainContent extends Component {
   render() {
@@ -13,7 +14,7 @@ class MainContent extends Component {
             Upverter Viewer makes it easy to share views of your design.
           </div>
           <div className="design-box">
-            <div className="d-flex justify-content-between align-items-center">
+            <div className="d-flex justify-content-between align-items-baseline">
               <h3>Share Your Design</h3>
               <img src={iconReplay} alt="ico ico-replay"/>
             </div>
@@ -21,6 +22,21 @@ class MainContent extends Component {
               Share your design via URL or embedded code. 
               Your recipients won't need to install software or sign in to view it.
             </p>
+            <div className="line"><div className="active-line"></div></div>
+          </div>
+          <div className="design-box">
+            <div className="d-flex justify-content-between align-items-baseline">
+              <h3>All Design Formats</h3>
+              <img src={iconPlus} alt="ico ico-replay"/>
+            </div>
+            <div className="line"></div>
+          </div>
+          <div className="design-box">
+            <div className="d-flex justify-content-between align-items-baseline">
+              <h3>Get Feedback Faster</h3>
+              <img src={iconPlus} alt="ico ico-replay"/>
+            </div>
+            <div className="line"></div>
           </div>
         </div>
       </div>
