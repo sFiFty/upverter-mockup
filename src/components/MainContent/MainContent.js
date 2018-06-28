77c5dd9b35ff05e333f3f36622c9f04c19504651
sFiFty <alexandr.rudin.my@gmail.com>
2018-06-28 09:41:49 +0300

    added icons

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -4,12 +4,19 @@ import './styles.css';
 class MainContent extends Component {
   render() {
     return (
-      <div className="main-content">
+      <div className="main-content d-flex flex-column justify-content-center">
         <div className="text-content">
           <h1>Share, screenshot and print.</h1>
           <div className="subhead">
             Upverter Viewer makes it easy to share views of your design.
           </div>
+        </div>
+        <div className="design-box">
+          <h3>Share Your Design</h3>
+          <p>
+            Share your design via URL or embedded code. 
+            Your recipients won't need to install software or sign in to view it.
+          </p>
         </div>
       </div>
     );
